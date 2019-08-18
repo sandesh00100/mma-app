fb6ba5ce0eb16f45ea2dda86035b463f6f2a5570
sandesh <sandesh.shrestha@uconn.edu>
2019-08-18 11:19:26 -0400

    installing dependencies and setting up backend for development

---
+++ mma-app/backend/app.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const mongoose = require('mongoose');
+// secure password relative to app.js
+const mongoPassword = require('../../../pas');
+// 'node-angular' is the database it is storing the post in
+const databaseURL = 'mongodb+srv://sandesh:' + mongoPassword.PASSWORD + '@mean-stack-optfw.mongodb.net/node-angular?retryWrites=true';
+
+mongoose.connect(databaseURL, { useNewUrlParser: true })
+  .then(() => {
+    console.log('\nConnected to mongo database!\n');
+  }).catch(() => {
+    console.log('Connection failed and the password is');
+  });
+
+app.use(bodyParser.json());
+
+// Any requests targeting the images folder should be allowed (probably because it's relative to the server.js)
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+    // no matter what the app domain is running on you are able to use this app
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    // what http requests can be sent
+    res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, PUT, OPTIONS");
+    next();
+  });
+
+  module.exports = app;
