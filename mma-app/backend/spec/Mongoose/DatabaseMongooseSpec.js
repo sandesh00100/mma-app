428a4ab8eb1fb65172a9298544f86291d1979f33
Sandesh Shrestha <sandesh.shrestha@uconn.edu>
2019-08-31 08:26:07 -0400

    basically done with mock data, need to test

--- mma-app/backend/spec/Mongoose/DatabaseMongooseSpec.js
+++ mma-app/backend/spec/Mongoose/DatabaseMongooseSpec.js
@@ -4,173 +4,188 @@ const FighterModel = require("../../models/fighter.model");
 const JudgeModel = require('../../models/judge.model');
 
 describe('Mongo Database Models', () => {
-    let fighterObj;
-    let fighter2Obj;
-    let fighter1;
-    let fighter2;
-    let matchObj;
+  let fighterObj;
+  let fighter2Obj;
+  let fighter1;
+  let fighter2;
+  let matchObj;
 
-    beforeEach(() => {
-        fighterObj = {
-            firstName: "Sandesh",
-            lastName: "Shrestha",
-            isActive: true,
-            imagePath: "/somepath.png",
-            record: {
-                wins: 1,
-                losses: 1,
-                draws: 1,
-                disqualifications: 1,
-            },
-            isTestData: true
-        };
 
-        fighter2Obj = { ...fighterObj };
-        fighter2Obj.firstName = "Steve";
+  beforeAll(done => {
+    const mongoose = require('mongoose');
+    const mongoPassword = require('../../../../../pas');
+    // 'node-angular' is the database it is storing the post in
+    const databaseURL = 'mongodb+srv://sandesh:' + mongoPassword.PASSWORD + '@mean-stack-optfw.mongodb.net/node-angular?retryWrites=true';
+    mongoose.connect(databaseURL, { useNewUrlParser: true, useCreateIndex: true })
+      .then(() => {
+        console.info('\n*Connected to mongo\n');
+        done();
+      }).catch(() => {
+        console.info('\n*Connection failed\n');
+      });
+  });
 
-        fighter1 = new FighterModel(fighterObj);
-        fighter2 = new FighterModel(fighter2Obj);
+  beforeEach(() => {
+    fighterObj = {
+      firstName: "Sandesh",
+      lastName: "Shrestha",
+      isActive: true,
+      imagePath: "/somepath.png",
+      record: {
+        wins: 1,
+        losses: 1,
+        draws: 1,
+        disqualifications: 1,
+      },
+      isTestData: true
+    };
 
-        matchObj = {
-            eventName: "UFC 242",
-            organization: "UFC",
-            weightClass: 155,
-            matchType: "Main",
-            matchOrder: 1,
-            isFiveRounds: true,
-            fighters: [],
-            isTestData: true
-        }
+    fighter2Obj = { ...fighterObj };
+    fighter2Obj.firstName = "Steve";
+
+    fighter1 = new FighterModel(fighterObj);
+    fighter2 = new FighterModel(fighter2Obj);
+
+    matchObj = {
+      eventName: "UFC 242",
+      organization: "UFC",
+      weightClass: 155,
+      matchType: "Main",
+      matchOrder: 1,
+      isFiveRounds: true,
+      fighters: [],
+      isTestData: true
+    }
+  });
+
+  afterAll((done) => {
+    console.info("\t* Deleting test data: ");
+
+    // Maybe use return statemetns for these
+    FighterModel.deleteMany({ isTestData: true }).then((deletedFightsResponse) => {
+      console.info("\t\t* Num Fighters: " + deletedFightsResponse.n);
+      MatchModel.deleteMany({ isTestData: true }).then((deletedMatchesResponse) => {
+        console.info("\t\t* Num Matches: " + deletedMatchesResponse.n);
+        JudgeModel.deleteMany({ isTestData: true }).then(deletedJudgeResponse => {
+          console.info("\t\t* Num Judges: " + deletedJudgeResponse.n);
+          done();
+        });
+      });
     });
 
-    afterAll((done) => {
-        console.info("\t* Deleting test data: ");
 
-        // Maybe use return statemetns for these
-        FighterModel.deleteMany({ isTestData: true }).then((deletedFightsResponse) => {
-            console.info("\t\t* Num Fighters: " + deletedFightsResponse.n);
-            MatchModel.deleteMany({ isTestData: true }).then((deletedMatchesResponse) => {
-                console.info("\t\t* Num Matches: " + deletedMatchesResponse.n);
-                JudgeModel.deleteMany({ isTestData: true }).then(deletedJudgeResponse => {
-                    console.info("\t\t* Num Judges: " + deletedJudgeResponse.n);
+  });
+
+  it('Creates new fighters', (done) => {
+    fighter1.save().then((savedFighter) => {
+      expect(savedFighter.firstName).toBe("Sandesh");
+      fighter2.save().then(savedFighter2 => {
+        expect(savedFighter2.firstName).toBe("Steve");
+        done();
+      });
+    });
+  });
+
+  it('Creates a match', done => {
+    fighter1.save().then((savedFighter1) => {
+      matchObj.fighters.push(savedFighter1._id);
+      fighter2.save().then((savedFighter2) => {
+        matchObj.fighters.push(savedFighter2._id);
+        const match = new MatchModel(matchObj);
+        match.save().then(savedMatch => {
+          expect(savedMatch.fighters.length).toBe(2);
+          expect(match.eventName).toBe("UFC 242");
+          MatchModel.findOne({ eventName: "UFC 242" }).populate('fighters').exec((err, foundMatch) => {
+            if (err) {
+              console.info(err);
+            } else {
+              expect(foundMatch.fighters[0].firstName).not.toBe(null);
+            }
+            done();
+          });
+        }).catch(err => {
+          console.info(err);
+          done();
+        });
+      });
+    });
+  });
+
+  it('Creates a Judge', done => {
+    let judgeObj = {
+      email: "sandesh@test.com",
+      password: "testPassword",
+      matches: [],
+      isTestData: true
+    };
+
+    let roundsScoredObj1 = [];
+    let roundsScoredObj2 = [];
+
+    for (let i = 0; i < 5; i++) {
+      roundsScoredObj1.push({
+        takeDownAttempts: i,
+        takeDownDefense: i,
+        significantStrikes: i,
+        octagonControl: i,
+        damageRatio: i,
+        submissionAttempts: i,
+        // Random number between 7-10
+        score: Math.floor(Math.random() * (10 - 7)) + 7,
+        roundNumber: i + 1
+      });
+      roundsScoredObj2.push({
+        takeDownAttempts: i,
+        takeDownDefense: i,
+        significantStrikes: i,
+        octagonControl: i,
+        damageRatio: i,
+        submissionAttempts: i,
+        score: Math.floor(Math.random() * (10 - 7)) + 7,
+        roundNumber: i + 1
+      });
+    }
+
+    fighter1.save().then(savedFighter1 => {
+      matchObj.fighters.push(savedFighter1._id);
+      fighter2.save().then(savedFighter2 => {
+        matchObj.fighters.push(savedFighter2._id);
+        const testMatch = new MatchModel(matchObj);
+        testMatch.save().then(savedMatch => {
+          MatchModel.findById(savedMatch._id).populate('fighters').exec((err, popMatch) => {
+            const judgeMatchObj = {
+              match: popMatch._id,
+              roundsScored: [
+                {
+                  fighter: popMatch.fighters[0]._id,
+                  rounds: roundsScoredObj1
+                },
+                {
+                  fighter: popMatch.fighters[1]._id,
+                  rounds: roundsScoredObj2
+                }
+              ]
+            };
+
+            judgeObj.matches.push(judgeMatchObj);
+            const judge = new JudgeModel(judgeObj);
+            judge.save().then(savedJudge => {
+              JudgeModel.findById(savedJudge)
+                .populate('match')
+                .populate('fighter')
+                .exec((err, foundJudge) => {
+                  if (!err) {
+                    expect(foundJudge.email).toBe('sandesh@test.com');
+                    expect(foundJudge.matches.length).toEqual(1);
                     done();
+                  }
                 });
             });
+          });
+
         });
-
-
+      });
     });
-
-    it('Creates new fighters', (done) => {
-        fighter1.save().then((savedFighter) => {
-            expect(savedFighter.firstName).toBe("Sandesh");
-            fighter2.save().then(savedFighter2 => {
-                expect(savedFighter2.firstName).toBe("Steve");
-                done();
-            });
-        });
-    });
-
-    it('Creates a match', done => {
-        fighter1.save().then((savedFighter1) => {
-            matchObj.fighters.push(savedFighter1._id);
-            fighter2.save().then((savedFighter2) => {
-                matchObj.fighters.push(savedFighter2._id);
-                const match = new MatchModel(matchObj);
-                match.save().then(savedMatch => {
-                    expect(savedMatch.fighters.length).toBe(2);
-                    expect(match.eventName).toBe("UFC 242");
-                    MatchModel.findOne({ eventName: "UFC 242" }).populate('fighters').exec((err, foundMatch) => {
-                        if (err) {
-                            console.info(err);
-                        } else {
-                            expect(foundMatch.fighters[0].firstName).not.toBe(null);
-                        }
-                        done();
-                    });
-                }).catch(err => {
-                    console.info(err);
-                    done();
-                });
-            });
-        });
-    });
-
-    it('Creates a Judge', done => {
-        let judgeObj = {
-            email: "sandesh@test.com",
-            password: "testPassword",
-            matches: [],
-            isTestData: true
-        };
-
-        let roundsScoredObj1 = [];
-        let roundsScoredObj2 = [];
-
-        for (let i = 0; i < 5; i++) {
-            roundsScoredObj1.push({
-                takeDownAttempts: i,
-                takeDownDefense: i,
-                significantStrikes: i,
-                octagonControl: i,
-                damageRatio: i,
-                submissionAttempts: i,
-                // Random number between 7-10
-                score: Math.floor(Math.random() * (10 - 7)) + 7,
-                roundNumber: i + 1
-            });
-            roundsScoredObj2.push({
-                takeDownAttempts: i,
-                takeDownDefense: i,
-                significantStrikes: i,
-                octagonControl: i,
-                damageRatio: i,
-                submissionAttempts: i,
-                score: Math.floor(Math.random() * (10 - 7)) + 7,
-                roundNumber: i + 1
-            });
-        }
-
-        fighter1.save().then(savedFighter1 => {
-            matchObj.fighters.push(savedFighter1._id);
-            fighter2.save().then(savedFighter2 => {
-                matchObj.fighters.push(savedFighter2._id);
-                const testMatch = new MatchModel(matchObj);
-                testMatch.save().then(savedMatch => {
-                    MatchModel.findById(savedMatch._id).populate('fighters').exec((err, popMatch) => {
-                        const judgeMatchObj = {
-                            match: popMatch._id,
-                            roundsScored: [
-                                {
-                                    fighter: popMatch.fighters[0]._id,
-                                    rounds: roundsScoredObj1
-                                },
-                                {
-                                    fighter: popMatch.fighters[1]._id,
-                                    rounds: roundsScoredObj2
-                                }
-                            ]
-                        };
-
-                        judgeObj.matches.push(judgeMatchObj);
-                        const judge = new JudgeModel(judgeObj);
-                        judge.save().then(savedJudge => {
-                            JudgeModel.findById(savedJudge)
-                                .populate('match')
-                                .populate('fighter')
-                                .exec((err, foundJudge) => {
-                                    if (!err) {
-                                        expect(foundJudge.email).toBe('sandesh@test.com');
-                                        expect(foundJudge.matches.length).toEqual(1);
-                                        done();
-                                    }
-                                });
-                        });
-                    });
-
-                });
-            });
-        });
-    });
+  });
 
 });
--- mma-app/backend/tools/dataCreationTool.js
+++ mma-app/backend/tools/dataCreationTool.js
@@ -9,63 +9,107 @@ let fighterObjects = [];
 let matchObjects = [];
 let matchesPerEvents;
 let eventHelper = {
-    ufcEvents:[],
-    bellatorEvents:[]
+  ufcEvents: [],
+  bellatorEvents: []
 };
 
 const orgs = ['UFC', 'Bellator'];
 const matchType = ['Main', 'Prelims', 'Early Prelims'];
 const weightClasses = [125, 135, 145, 155, 170, 185, 205, 265];
 
+// Return an object with 2 unique fighter IDs
 const getFighters = () => {
+  const randomFighterIndex = randomNumber(0, fighterObjects.length - 1);
+  let randomFighterIndex2 = randomNumber(0, fighterObjects.length - 1);
+  if (randomFighterIndex == randomFighterIndex2) {
+    randomFighterIndex2 = (randomFighterIndex2 + 1) % fighterObjects.length;
+  }
+  const fighter1Id = fighterObjects[randomFighterIndex]._id;
+  const fighter2Id = fighterObjects[randomFighterIndex2]._id;
 
-}
+  return {
+    fighter1Id: fighter1Id,
+    fighter2Id: fighter2Id
+  };
+};
+
+const isTitleFight = (matchOrder) => {
+  if ((matchOrder == 0) && (randomNumber(0, 1) == 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isFiveRounder = (matchOrder, isTitleFight) => {
+  if (isTitleFight || matchOrder == 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const createMockData = async (numFighters, numMatches, numEvents, numJudges) => {
 
-    // Create fighters with empty records
-    for (let i = 0; i < numFighters; i++) {
-        const fighter = new FighterModel({
-            firstName: "firstName" + i,
-            lastName: "lastName" + i,
-            isActive: true,
-            imagePath: "imagePath" + i,
-            record: {
-                wins: 0,
-                losses: 0,
-                draws: 0,
-                disqualifications: 0
-            },
+  // Create fighters with empty records
+  for (let i = 0; i < numFighters; i++) {
+    const fighterObj = {
+      firstName: "firstName" + i,
+      lastName: "lastName" + i,
+      isActive: true,
+      imagePath: "imagePath" + i,
+      record: {
+        wins: 0,
+        losses: 0,
+        draws: 0,
+        disqualifications: 0
+      },
 
-            //TODO: REMOVE THIS WHEN READY TO SAVE
-            _id: i,
-            isTestData: true
-        });
+      //TODO: REMOVE THIS WHEN READY TO SAVE
+      _id: i,
+      isTestData: true
+    };
+    const fighter = new FighterModel(fighterObj);
 
-        // Save fighter and keep it in memory
-        // const savedFighter = await fighter.save();
-        fighterObjects.push(savedFighter);
+    // Save fighter and keep it in memory
+    // const savedFighter = await fighter.save();
+    fighterObjects.push(savedFighter);
+  };
+
+  let eventIndex = 0;
+  let matchTypeIndex = 0;
+  const numMatchesPerEvents = numMatches / numEvents;
+  const numMatchesPerMatchType = numMatchesPerEvents / 3;
+  let currentEvent = [];
+
+  for (let i = 0; i < numMatches; i++) {
+    // Randomly weightClass and org
+    const selectedWeightClass = weightClasses[randomNumber(0, weightClasses.length - 1)];
+    const selectedOrg = orgs[randomNumber(0, orgs.length - 1)];
+    const selectedFighters = getFighters();
+    // Equally distribute matches between events
+    const selectedMatchType = matchType[i / numMatchesPerMatchType];
+    const eventNumber = i / numMatchesPerEvents;
+    const matchOrder = i % numMatchesPerEvents;
+    const titleFightFlag = isTitleFight(matchOrder);
+    const fiveRounderFlag = isFiveRounder(matchOrder, isTitleFight);
+
+    const matchObj = {
+      eventName: selectedOrg + " " + eventNumber,
+      organizaion: selectedOrg,
+      weightClass: selectedWeightClass,
+      matchType: selectedMatchType,
+      matchOrder: matchOrder,
+      isTitleFight: titleFightFlag,
+      isFiveRounder: fiveRounderFlag,
+      fighters: [selectedFighters.fighter1Id, selectedFighters.fighter2Id],
+      isTestData: true
     };
 
-    let eventIndex = 0;
-    let matchTypeIndex = 0;
-    const numMatchesPerEvents = numMatches/numEvents;
-    const numMatchesPerMatchType = numMatchesPerEvents/3;
-    let currentEvent = [];
+    const match = new Match(matchObj);
+  }
 
-    for (let i = 0; i < numMatches; i++) {
-        // Randomly weightClass and org
-        const selectedWeightClass = weightClasses[randomNumber(0, weightClasses.length - 1)];
-        const selectedOrg = orgs[randomNumber(0, orgs.length - 1)];
-
-        // Equally distribute matches between events
-        const selectedMatchType = matchType[i/numMatchesPerMatchType];
-        const eventNumber = i/numMatchesPerEvents;
-        const matchOrder = i%numMatchesPerEvents;
-
-        const match = new Match();
-    }
-
-    console.log(fighterObjects);
+  console.log(fighterObjects);
 };
 
 createMockData(50, 90, 5, 10);
