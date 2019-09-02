472cf36c110c99fa84634cfc9bcbda1e82a9760a
sandesh <sandesh.shrestha@uconn.edu>
2019-09-02 18:07:21 -0400

    Starting to work on the front end (match view)

--- mma-app/src/app/app.module.ts
+++ mma-app/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule, MatTabsModule, MatCardModule, MatExpansionModule, MatButtonModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatchesComponent } from './components/matches/matches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MatchesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
