50fb7c9ef9cf88b23aa5c9092c8b221a39ca3cd0
sandesh <sandesh.shrestha@uconn.edu>
2019-09-09 21:38:05 -0400

    Styling sucks, still got lots to do but added some stats on the accordian

--- mma-app/src/app/app.module.ts
+++ mma-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MatToolbarModule, MatTabsModule, MatCardModule, MatExpansionModule, MatButtonModule, MatPaginatorModule} from '@angular/material';
+import { MatToolbarModule, MatTabsModule, MatCardModule, MatExpansionModule, MatButtonModule, MatPaginatorModule, MatDividerModule, MatListModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatchesComponent } from './components/matches/matches.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -22,7 +22,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatExpansionModule,
     MatButtonModule,
     MatPaginatorModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
