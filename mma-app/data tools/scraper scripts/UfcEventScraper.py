17b7522161c9ab581ca7ed0a11d7939cdcd81a40
sandesh <sandesh.shrestha@uconn.edu>
2019-11-29 10:46:06 -0500

    Handeling pages with multiple match tables

--- mma-app/data tools/scraper scripts/UfcEventScraper.py
+++ mma-app/data tools/scraper scripts/UfcEventScraper.py
@@ -12,18 +12,61 @@ ROW_TAG = "tr"
 TABLE_DATA_TAG = "td"
 
 PARSER_TYPE = "html.parser"
-UFC_EVENT_LIST_CSV_PATH =  "../data/UFC/UFC_Event_List.csv"
-TABLE_HEADER_SET = set(["Weight class","Method","Round","Time","Notes"])
+UFC_EVENT_LIST_CSV_PATH =  "../data/UFC/UFC_Event_List_Alt.csv"
+FIGHT_TABLE_HEADER_SET = set(["Weight class","Method","Round","Time","Notes"])
+INFO_TABLE_HEADER_SET = set(["Promotion","Date","Venue","Attendance","Total gate"])
+DATE_HEADER = "Date"
 
-def getFightTable(pageTables):
-        for table in pageTables:
-            rows = table.find_all(ROW_TAG)
-            for row in rows:
-                headers = row.find_all(HEADER_TAG)
-                for header in headers:
-                    headerText = header.get_text().strip()
-                    if headerText in TABLE_HEADER_SET:
-                        return table
+def isFightTable(table):
+    rows = table.find_all(ROW_TAG)
+    for row in rows:
+        headers = row.find_all(HEADER_TAG)
+        if len(headers) > 8:
+            return False
+        for header in headers:
+            headerText = header.get_text().strip()
+            if headerText in FIGHT_TABLE_HEADER_SET:
+                return True
+    return False
+
+def isInfoTable(table):
+    rows = table.find_all(ROW_TAG)
+    for row in rows:
+        headers = row.find_all(HEADER_TAG)
+        for header in headers:
+            headerText = header.get_text().strip()
+            if headerText in INFO_TABLE_HEADER_SET:
+                return True
+    return False
+
+def getInfoTableDate(table):
+    rows = table.find_all(ROW_TAG)
+    for row in rows:
+        header = row.find(HEADER_TAG)
+        if header == DATE_HEADER:
+            date = row.find(TABLE_DATA_TAG)
+            return date
+
+def writeTableCsv(table, eventFilePath):
+    with open(eventFilePath, 'w', newline = '', encoding="utf-8") as eventCsv:
+        headers = ['Weight Class','Red Fighter','Outcome', 'Blue Fighter','Method', 'Round', 'Time']
+        csvWriter = csv.DictWriter(eventCsv, fieldnames=headers, delimiter=",")
+        csvWriter.writeheader()
+        rows = table.find_all(ROW_TAG)
+
+        for row in rows:
+            tableData = row.find_all(TABLE_DATA_TAG)
+            if len(tableData) == 8:
+                rowDict = dict()
+                rowDict["Weight Class"] = tableData[0].get_text().strip()
+                rowDict["Red Fighter"] = tableData[1].get_text().strip()
+                rowDict["Outcome"] = tableData[2].get_text().strip()
+                rowDict["Blue Fighter"] = tableData[3].get_text().strip()
+                rowDict["Method"] = tableData[4].get_text().strip()
+                rowDict["Round"] = tableData[5].get_text().strip()
+                rowDict["Time"] = tableData[6].get_text().strip()
+
+                csvWriter.writerow(rowDict)
 
 try:
     with open(UFC_EVENT_LIST_CSV_PATH,'r') as ufcEventListCsv:
@@ -41,35 +84,34 @@ try:
                 soup = BeautifulSoup(pageHTML,PARSER_TYPE)
 
                 pageTables = soup.find_all(TABLE_TAG)
-                resultsTable = getFightTable(pageTables)
-                
-                rows = resultsTable.find_all(ROW_TAG)
+
+                validResultsTables = []
+                validInfoTables = []
+                infoTableDates = []
+
+                for table in pageTables:
+                    if isFightTable(table):
+                        validResultsTables.append(table)
+                    elif isInfoTable(table):
+                        validInfoTables.append(table)
+                        date = getInfoTableDate(table)
+                        infoTableDates.append(date)
+
                 eventFolderPath = "../data/UFC/UFC Events"
-                fileName = eventDate.replace(' ','')
-                eventFilePath = eventFolderPath + "/" + fileName + ".csv"
-
-                with open(eventFilePath, 'w', newline = '', encoding="utf-8") as eventCsv:
-                    headers = ['Weight Class','Red Fighter','Outcome', 'Blue Fighter','Method', 'Round', 'Time']
-                    csvWriter = csv.DictWriter(eventCsv, fieldnames=headers, delimiter=",")
-                    csvWriter.writeheader()
-
-                    for row in rows:
-                        tableData = row.find_all(TABLE_DATA_TAG)
-                        if len(tableData) == 8:
-                            rowDict = dict()
-                            rowDict["Weight Class"] = tableData[0].get_text().strip()
-                            rowDict["Red Fighter"] = tableData[1].get_text().strip()
-                            rowDict["Outcome"] = tableData[2].get_text().strip()
-                            rowDict["Blue Fighter"] = tableData[3].get_text().strip()
-                            rowDict["Method"] = tableData[4].get_text().strip()
-                            rowDict["Round"] = tableData[5].get_text().strip()
-                            rowDict["Time"] = tableData[6].get_text().strip()
-
-                            csvWriter.writerow(rowDict)
+                
+                if len(validResultsTables) > 1:
+                    for i, validResultsTable in enumerate(validResultsTables):
+                        infoDate = infoTableDates[i]
+                        fileName = infoDate.replace(' ','')
+                        eventFilePath = eventFolderPath + "/" + fileName + ".csv"
+                        writeTableCsv(resultsTable,eventFilePath)
+                else:
+                    resultsTable = validResultsTables[0]
+                    fileName = eventDate.replace(' ','')
+                    eventFilePath = eventFolderPath + "/" + fileName + ".csv"
+                    writeTableCsv(resultsTable, eventFilePath)
             
             time.sleep(2)
-
-                        
 
 except error.HTTPError:
     print("Can't Access UFC fighter WIKI")
