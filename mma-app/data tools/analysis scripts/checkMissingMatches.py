f985a84876695d38b4045fbd9de6f832d566e104
sandesh <sandesh.shrestha@uconn.edu>
2019-12-05 21:11:24 -0500

    Using helper scripts to generate final data set pt 2. Last commit didn't include everything.

---
+++ mma-app/data tools/analysis scripts/checkMissingMatches.py
@@ -0,0 +1,22 @@
+import csv
+import os
+
+with open("../data/UFC/UFC_Event_List.csv") as ufcEventList:
+    csvReader = csv.DictReader(ufcEventList)
+    eventListDateSet = set()
+    
+    for row in csvReader:
+        modifiedDate = row["Date"].replace(' ','')
+
+        if modifiedDate in eventListDateSet:
+            print(modifiedDate + " is a duplicate event list date")
+            
+        eventListDateSet.add(modifiedDate) 
+    
+    csvMatchFiles =  os.listdir("../data/UFC/UFC Events")
+
+    for matchFile in csvMatchFiles:
+        matchFileDate = matchFile.split('.')[0]
+        if matchFileDate not in eventListDateSet:
+            print(matchFileDate + " not in UFC Event List")
+    
---
+++ mma-app/data tools/conversion scripts/ufcEventListConverter.py
@@ -0,0 +1,16 @@
+import csv
+import os
+
+UFC_EVENTS_DIR = "../data/UFC/UFC Events"
+with open("../data/UFC/UFC_Event_List.csv",'r') as ufcEventListCsv:
+    csvReader = csv.DictReader(ufcEventListCsv)
+    for row in csvReader:
+        modifiedDate = modifiedDate = row["Date"].replace(' ','')
+        fileName = modifiedDate+".csv"
+        csvFileNames = os.listdir(UFC_EVENTS_DIR)
+
+        if fileName in csvFileNames:
+            newFileName = row["Number"] +","+fileName
+            os.rename(UFC_EVENTS_DIR+"/"+fileName,UFC_EVENTS_DIR+"/"+newFileName)
+        else:
+            print(fileName + " file not in directory")
