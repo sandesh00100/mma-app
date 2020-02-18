e8e03eb50071bbec0749d0e16158f18c031304d9
sandesh <sandesh.shrestha@uconn.edu>
2020-02-17 22:03:24 -0500

    uncommenting syncDb script

--- mma-app/backend/tools/data tools/data creation scripts/syncDatabase.bat
+++ mma-app/backend/tools/data tools/data creation scripts/syncDatabase.bat
@@ -1,12 +1,12 @@
 @echo off
-REM cd "../scraper scripts"
-REM echo Scraping event list
-REM python UfcEventListScraper.py
-REM echo Scraping event data
-REM python UfcEventScraper.py
-REM cd "../conversion scripts"
-REM echo Converting eventDataFilesToJson
-REM python eventDataFilesToJson.py
-REM cd "../data creation scripts"
+cd "../scraper scripts"
+echo Scraping event list
+python UfcEventListScraper.py
+echo Scraping event data
+python UfcEventScraper.py
+cd "../conversion scripts"
+echo Converting eventDataFilesToJson
+python eventDataFilesToJson.py
+cd "../data creation scripts"
 echo Syncing database with event json file
 node dataCreationTool.js
